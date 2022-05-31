0cc889b1b94c95f1087cfa69541395aa3aa7c195
Xin Vance <xinpengfei520@gmail.com>
2022-05-31 15:16:08 +0800

    完成底部TAB bar 逻辑

--- iOS-SampleApp/AppDelegate.m
+++ iOS-SampleApp/AppDelegate.m
@@ -16,6 +16,41 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    
+    UITabBarController *tabBarController= [[UITabBarController alloc]init];
+    tabBarController.tabBar.backgroundColor = [UIColor grayColor];
+    
+    UIViewController *controller1 = [[UIViewController alloc]init];
+    controller1.view.backgroundColor = [UIColor redColor];
+    controller1.tabBarItem.title = @"新闻";
+    controller1.tabBarItem.image = [UIImage imageNamed:@"icon.bundle/page@2x.png"];
+    controller1.tabBarItem.selectedImage = [UIImage imageNamed:@"icon.bundle/page_selected@2x.png"];
+    
+    UIViewController *controller2 = [[UIViewController alloc]init];
+    controller2.view.backgroundColor = [UIColor yellowColor];
+    controller2.tabBarItem.title = @"视频";
+    controller2.tabBarItem.image = [UIImage imageNamed:@"icon.bundle/video@2x.png"];
+    controller2.tabBarItem.selectedImage = [UIImage imageNamed:@"icon.bundle/video_selected@2x.png"];
+    
+    UIViewController *controller3 = [[UIViewController alloc]init];
+    controller3.view.backgroundColor = [UIColor greenColor];
+    controller3.tabBarItem.title = @"推荐";
+    controller3.tabBarItem.image = [UIImage imageNamed:@"icon.bundle/like@2x.png"];
+    controller3.tabBarItem.selectedImage = [UIImage imageNamed:@"icon.bundle/like_selected@2x.png"];
+    
+    UIViewController *controller4 = [[UIViewController alloc]init];
+    controller4.view.backgroundColor = [UIColor blueColor];
+    controller4.tabBarItem.title = @"我的";
+    controller4.tabBarItem.image = [UIImage imageNamed:@"icon.bundle/home@2x.png"];
+    controller4.tabBarItem.selectedImage = [UIImage imageNamed:@"icon.bundle/home_selected@2x.png"];
+    
+    [tabBarController setViewControllers:@[controller1,controller2,controller3,controller4]];
+    
+    self.window.rootViewController = tabBarController;
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
@@ -23,18 +58,18 @@
 #pragma mark - UISceneSession lifecycle
 
 
-- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
-    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
-}
-
-
-- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-}
+//- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
+//    // Called when a new scene session is being created.
+//    // Use this method to select a configuration to create the new scene with.
+//    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
+//}
+//
+//
+//- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
+//    // Called when the user discards a scene session.
+//    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+//    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+//}
 
 
 @end
--- iOS-SampleApp/ViewController.m
+++ iOS-SampleApp/ViewController.m
@@ -7,23 +7,92 @@
 
 #import "ViewController.h"
 
+@interface TestView : UIView
+
+@end
+
+@implementation TestView
+
+- (instancetype)init{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+- (void)willMoveToSuperview:(nullable UIView *)newSuperview{
+    [super willMoveToSuperview:newSuperview];
+}
+
+- (void)didMoveToSuperview{
+    [super didMoveToSuperview];
+}
+
+- (void)willMoveToWindow:(nullable UIWindow *)newWindow{
+    [super willMoveToWindow:newWindow];
+}
+
+- (void)didMoveToWindow{
+    [super didMoveToWindow];
+}
+
+@end
+
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
 
+- (instancetype)init{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated{
+    [super viewDidDisappear:animated];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
     // Do any additional setup after loading the view.
     
-    [self.view addSubview:({
-            UILabel *label  = [[UILabel alloc] init];
-            label.text = @"Hello World!";
-            [label sizeToFit];
-            label.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
-            label;
-    })];
+    // hello world
+//    [self.view addSubview:({
+//            UILabel *label  = [[UILabel alloc] init];
+//            label.text = @"Hello World!";
+//            [label sizeToFit];
+//            label.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
+//            label;
+//    })];
+    
+//    UIView *view = [[UIView alloc]init];
+//    view.backgroundColor = [UIColor redColor];
+//    view.frame = CGRectMake(100, 100, 100, 100);
+//    [self.view addSubview:view];
+    
+    TestView *view2 = [[TestView alloc]init];
+    view2.backgroundColor = [UIColor greenColor];
+    view2.frame = CGRectMake(150, 150, 100, 100);
+    [self.view addSubview:view2];
+    
 }
 
 
